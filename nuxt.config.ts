3f627a11ad60d0361dd858df254e12c17d8fb850
SuperSonic <supersonic@livemail.tw>
2024-01-02 14:44:23 +0800

    feat: add titles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,14 @@
 export default defineNuxtConfig({
   app: {
     head: {
+      title: '看板',
+      titleTemplate: '%s - Deter 迪特',
+      meta: [
+        {
+          name: 'description',
+          content: '在名為 Discord 的高速公路風馳電掣的論壇系統！ 在 Discord 建立公開論壇？騙人的吧...'
+        }
+      ],
       link: [
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;500;700&display=swap' },
         { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/tocas/4.2.5/tocas.min.css' },
