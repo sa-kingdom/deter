e0e5ed036c15d76c308a11bccd2d40ff048b5980
不知火 Shiranui <supersonic@livemail.tw>
2026-08-31 02:30:44 +0800

    feat(collections): wire collections sidebar and add collection filter to discussions API

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,31 @@
 <template>
   <div>
-    <index-discussion-item v-for="(j, i) in data" :key="i" v-bind="j" />
+    <index-discussion-item
+      v-for="(j, i) in data"
+      :key="i"
+      v-bind="j"
+    />
   </div>
 </template>
 
 <script setup>
+import {computed, watch} from 'vue';
 import IndexDiscussionItem from '../components/IndexDiscussionItem.vue';
 
 const {apiInvokeBaseUrl} = useRuntimeConfig().public;
+const route = useRoute();
 
-const {data, error} = await useFetch(`${apiInvokeBaseUrl}/discussions`, {
-  key: 'discussions',
+const {data, error, refresh} = await useFetch(
+    `${apiInvokeBaseUrl}/discussions`,
+    {
+      query: computed(() => ({
+        collection: route.query.collection || undefined,
+      })),
+    },
+);
+
+watch(() => route.query.collection, () => {
+  refresh();
 });
 
 if (error.value) {
